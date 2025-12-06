31a0f22783056574ebe92c18f81c2dcf2e187e6e
Preetham1869 <23mh1a05l1@acoe.edu.in>
2025-12-06 11:31:21 +0530

    Enhance user update and delete endpoints with logging

--- index.js
+++ index.js
@@ -8,9 +8,21 @@ app.get('/',(req,res)=>{
     res.send('Api called successfully');
 })
 
+app.put('/update-user',(req,res)=>{
+    let data = req.body;
+    console.log(data);
+    res.end('User Updated successfully');
+})
+
 app.post('/add-user',(req,res)=>{
     let data = req.body;//Logic to add data to DB
+    console.log(data);
     res.send('Data Added called successfully');
+})
+
+app.delete('/delete-user',(req,res)=>{
+    console.log(req.body);
+    res.send('User Deleted successfully');
 })
 
 app.listen(3000, () => {
