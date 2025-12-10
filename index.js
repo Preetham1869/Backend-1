dfaa8611a64cf29e6fc9f49ddd6623f87eade569
Preetham1869 <23mh1a05l1@acoe.edu.in>
2025-12-10 11:16:43 +0530

    refactor: remove unnecessary blank line in index.js

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ import router from './Routes/studentsRoutes.js';
 const app = express();
 app.use(express.json());
 app.use('/', router);
-
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
